288241992a7a56fe691c77220b898772b6973583
Dario Stamenkovic <stamenkovicdario@gmail.com>
2024-12-01 20:37:50 +0100

    Finished conditionals exercises

--- root/index.js
+++ root/index.js
@@ -3,3 +3,12 @@ var start = "Hello World";
 var featureA = "featureA";
 
 var featureB = "featureB";
+
+//task1
+var task1 = "task1";
+
+//task2
+var task2 = "task2";
+
+//task3
+var task3 = "task3";
